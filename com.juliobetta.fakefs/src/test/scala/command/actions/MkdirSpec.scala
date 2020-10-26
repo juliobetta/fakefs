05c1c642048f15504b8ff6a1cc1ccf9b2c271fa6
Julio Betta <juliobetta@gmail.com>
2020-10-26 01:56:03 -0300

    mkdir initial implementation

---
+++ com.juliobetta.fakefs/src/test/scala/command/actions/MkdirSpec.scala
@@ -0,0 +1,36 @@
+package command.actions
+
+import app.State
+import fs.Directory
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+class MkdirSpec extends AnyFunSpec with Matchers with BeforeAndAfterEach {
+  describe("Mkdir") {
+    import fixtures.FileTree._
+
+    val initialState = State("dir03/dir02", root)
+
+    it("creates a new directory and updates state") {
+      val dirName = "newDir"
+      val (state, _) = Mkdir(Vector(dirName))(initialState)
+      val found = Directory.findEntryByPath(state.root, state.currentPath).collect { case dir: Directory => dir }
+
+      found.get.contents.map(_.name) must contain (dirName)
+      state.currentDirectory.contents.map(_.name) must contain (dirName)
+    }
+
+    describe("when passing in -p") {
+      ignore("create a new directory in the given path") {
+
+      }
+
+      describe("when path is not found") {
+        ignore("returns an error message and keeps the same state") {
+
+        }
+      }
+    }
+  }
+}
