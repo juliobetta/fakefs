dd781834d0c0d18754387dd472f1da6406a727b2
Julio Betta <juliobetta@gmail.com>
2020-10-28 00:58:36 -0300

    implement `ls` command

--- com.juliobetta.fakefs/src/main/scala/command/Input.scala
+++ com.juliobetta.fakefs/src/main/scala/command/Input.scala
@@ -1,6 +1,6 @@
 package command
 
-import command.actions.{Cd, Mkdir}
+import command.actions._
 
 case class Input(input: String) {
   val tokens: Vector[String] = input.split(" ").toVector
@@ -8,6 +8,7 @@ case class Input(input: String) {
   val command: Command = tokens.head match {
     case "cd" => Cd(tokens.tail)
     case "mkdir" => Mkdir(tokens.tail)
+    case "ls" => Ls(tokens.tail)
     case _ => Invalid
   }
 }
